99dbc5226396b1392b458ebd46910f687aff051c
sandipannas <sandisingha@gmail.com>
2025-07-20 09:09:24 +0530

    JWT for token generation and authentication

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/user.model.js'
+
+export const authJWT =async (req,res,next)=>{
+   try{
+    
+    const token = req.cookies.jwt;
+    if(!token){
+        return res.status(401).json({
+            massage:"Unauthorized user"
+        })
+    }
+
+    const decoded = jwt.verify(token,process.env.JWT_PASSWORD);
+    if(!decoded){
+        return res.status(401).json({
+            massage:"Unauthorized user"
+        })
+    }
+    const userId = decoded.userId;
+    const user = await User.findById(userId);
+
+    if(!user){
+        return res.status(404).json({message:"unauthorized user"});
+    }
+    req.user=user;
+    next();
+   }
+   catch(err){
+    console.log("failed to authenticate")
+    return res.status(500).json({
+        massage:"internal server error"
+    })
+   }
+}
---
+++ backend/src/util/JWT.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+
+export const generateJWT = (userId,res)=>{
+  
+    const token = jwt.sign({userId},process.env.JWT_PASSWORD,{
+        expiresIn:"7d"
+    })
+    
+    res.cookie("jwt",token,{
+        maxAge:7*24*60*60*1000,
+        httpOnly:true, //prevent XSS attacks cross-site scripting attacks
+        sameSite:"strict", //CSRF attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !=="development"
+    });
+
+    return token;
+}
+
