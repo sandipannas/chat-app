9f7cb2325a571b035f43ec1880fbe2f480bdeef2
Sandipan Singha <132623998+sandipannas@users.noreply.github.com>
2025-08-24 00:06:42 +0530

    Update socket.js

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -7,7 +7,9 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors: {
-        origin:["*"]
+        origin:["http://localhost:5173"],
+        methods:["GET","POST"],
+        credentials:true
     }
 })
 
