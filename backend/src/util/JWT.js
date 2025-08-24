e266f3a63bfd1b404bb937de74cbf88a59a17dbe
sandipannas <sandisingha@gmail.com>
2025-08-25 02:37:50 +0530

    changed some cookie settings

--- backend/src/util/JWT.js
+++ backend/src/util/JWT.js
@@ -9,8 +9,8 @@ export const generateJWT = (userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:7*24*60*60*1000,
         httpOnly:true, //prevent XSS attacks cross-site scripting attacks
-        sameSite:"none", //CSRF attacks cross-site request forgery attacks
-        secure: process.env.NODE_ENV !== "development" 
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : "none", // Use "lax" for development
+        secure: process.env.NODE_ENV !== "development" // Only secure in production
     });
 
     return token;
